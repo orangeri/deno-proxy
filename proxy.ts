0e218c5ed92f5258bd030c50824c163f48197484
orangeri <135411258+orangeri@users.noreply.github.com>
2024-07-24 19:33:38 +0700

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,12 @@
-Deno.serve(async (req) => {
-  const url = new URL(req.url)
-  return fetch("https://api.openai.com" + url.pathname, {
-    method: req.method,
-    headers: {
-      Authorization: req.headers.get('Authorization'),
-      "Content-Type": req.headers.get('Content-Type')
-    },
-    body: req.body
-  })
+Deno.serve(async (req) => {  
+  const baseURL = Deno.env.get("BASE_URL") || "https://api.openai.com"; // Fallback ke URL default jika BASE_URL tidak didefinisikan  
+  const url = new URL(req.url);  
+  return fetch(baseURL + url.pathname, {  
+    method: req.method,  
+    headers: {  
+      Authorization: req.headers.get('Authorization'),  
+      "Content-Type": req.headers.get('Content-Type')  
+    },  
+    body: req.body  
+  })  
 });
